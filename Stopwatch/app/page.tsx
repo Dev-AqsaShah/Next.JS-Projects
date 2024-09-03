9478e5cef7eb9ff54135e4212864f10db078781d
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-09-04 00:19:24 +0500

    Add files via upload

---
+++ Stopwatch/app/page.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import Timer from '@/Components/Timer'
+
+function page() {
+  return (
+    <>
+    <div>
+      <Timer />
+    </div>
+    </>
+  )
+}
+
+export default page
